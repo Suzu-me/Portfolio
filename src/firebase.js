20980cd0ae2e2d75c0c569c135b122b840e1931f
Suzu <77507323+Suzu-me@users.noreply.github.com>
2021-04-30 19:08:10 +0530

    Portfolio site

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD3KlL_bGS4A1Pl7cPj1TsgZyQdSOO78DQ",
+    authDomain: "suzu-sharma.firebaseapp.com",
+    projectId: "suzu-sharma",
+    storageBucket: "suzu-sharma.appspot.com",
+    messagingSenderId: "822474124338",
+    appId: "1:822474124338:web:ada7cd706abd00d0f1a02b",
+    measurementId: "G-WYWELF44SD"
+  };
+
+  
